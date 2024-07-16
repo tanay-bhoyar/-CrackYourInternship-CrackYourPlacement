a541fc0e33e3ab9a87187168e48c6421cc1dc21a
tanay-bhoyar <66437798+tanay-bhoyar@users.noreply.github.com>
2024-07-16 12:20:04 +0530

    Create Multiply two linked lists.java

---
+++ Multiply two linked lists.java
@@ -0,0 +1,38 @@
+/*Node is defined as
+class Node
+{
+    int data;
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class Solution {
+    public long multiplyTwoLists(Node l1,Node l2){
+          //add code here.
+  final long MOD = 1000000007;
+        long num1 = 0;
+        long num2 = 0;
+
+        // Calculate num1 with modulo operation
+        Node temp = l1;
+        while (temp != null) {
+            num1 = (num1 * 10 + temp.data) % MOD;
+            temp = temp.next;
+        }
+
+        // Calculate num2 with modulo operation
+        Node temp2 = l2;
+        while (temp2 != null) {
+            num2 = (num2 * 10 + temp2.data) % MOD;
+            temp2 = temp2.next;
+        }
+
+        // Multiply num1 and num2 and take modulo again
+        return (num1 * num2) % MOD;
+   
+   
+   }
+}
