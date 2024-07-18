882371b174ec90010fc47dab3712c9d22e95697b
tanay-bhoyar <66437798+tanay-bhoyar@users.noreply.github.com>
2024-07-18 19:55:53 +0530

    Create 200. Number of Islands.java

---
+++ 200. Number of Islands.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int ans=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    ans++;
+                    helper(grid,i,j);
+                }
+            }
+        }
+        return ans;
+    }
+    public void helper(char [][]grid,int i,int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        helper(grid,i+1,j);
+        helper(grid,i-1,j);
+        helper(grid,i,j+1);
+        helper(grid,i,j-1);
+    }
+}
